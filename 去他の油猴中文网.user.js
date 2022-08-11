7dcb4fec702bc26d47979460d62c0a5a710777d6
柒伍七 <2858727468@qq.com>
2022-08-11 16:57:08 +0800

    v1.0.3

--- 去他の油猴中文网.user.js
+++ 去他の油猴中文网.user.js
@@ -12,22 +12,22 @@
 // @homepage     https://github.com/s757129/FuckScripts
 // @license      MIT
 // ==/UserScript==
- 
+
 (function() {
     'use strict';
- 
+
     //unsafeWindow
     unsafeWindow.GM_addStyle = GM_addStyle;
- 
+
     //fuckStyle
     let fuckStyle = `
 .comiis_nv_pop,.a_f,.a_p{display: none; !important}
 .mn{width: 100%;height: 100%;left: 0;top:0;right:0;bottom: 0;margin: auto;}
     `;
- 
+
     //loading
     GM_addStyle(fuckStyle);
- 
+
     //李恒道哥哥牛逼！
     setTimeout(function() {
         let oldcopy = unsafeWindow.setCopy;
@@ -35,5 +35,5 @@
             return oldcopy.call(this,text.replace('\n(出处: 油猴中文网)\n',''), msg)
         }
     }, 1000);
- 
+
 })();
