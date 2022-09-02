21669303390ca6b3678084b30a057bd43541094e
柒伍七 <2858727468@qq.com>
2022-09-02 15:03:48 +0800

    Update 去他の粤工程校园网.user.js

--- 去他の粤工程校园网.user.js
+++ 去他の粤工程校园网.user.js
@@ -5,6 +5,7 @@
 // @description  自动登录粤工程西区宿舍校园网
 // @author       柒伍七
 // @match        *://10.10.0.76/*
+// @match        *://10.10.0.253/*
 // @icon         https://www.gdep.edu.cn/__local/1/78/44/67DBBD0E4AF7849771AEBA0A69A_8F5875B0_45D51.jpg?e=.jpg
 // @run-at       document-end
 // @homepage     https://github.com/s757129/FuckScripts
@@ -25,6 +26,6 @@
     //延时点击登录按钮
     setTimeout(function() {
         document.querySelector('#loginsubmit').click();
-    }, 100);
+    }, 250);
  
 })();
