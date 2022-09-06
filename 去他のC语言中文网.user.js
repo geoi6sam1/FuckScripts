03c6ae269c0fc82611d1c24aebebb57ddcb45b20
柒伍七 <2858727468@qq.com>
2022-09-06 15:22:59 +0800

    Update 去他のC语言中文网.user.js

--- 去他のC语言中文网.user.js
+++ 去他のC语言中文网.user.js
@@ -1,16 +1,15 @@
 // ==UserScript==
 // @name         去他のC语言中文网
-// @namespace    s757129
-// @version      1.1.3
+// @namespace    https://github.com/s757129/FuckScripts
+// @version      1.1.4
 // @description  屏蔽C语言中文网广告加菊部美化
 // @author       柒伍七
 // @match        *://c.biancheng.net/*
 // @match        *://m.biancheng.net/*
 // @match        *://vip.biancheng.net/*
 // @icon         http://c.biancheng.net/favicon.ico
-// @require      https://unpkg.com/jquery@3.6.0/dist/jquery.min.js
-// @require      https://unpkg.com/sweetalert2@11.4.7/dist/sweetalert2.min.js
-// @resource     SwalCSS https://unpkg.com/sweetalert2@11.4.7/dist/sweetalert2.min.css
+// @require      https://unpkg.com/sweetalert2@11.4.30/dist/sweetalert2.min.js
+// @resource     SwalCSS https://unpkg.com/sweetalert2@11.4.30/dist/sweetalert2.min.css
 // @run-at       document-start
 // @grant        unsafeWindow
 // @grant        GM_addStyle
@@ -18,7 +17,7 @@
 // @grant        GM_setValue
 // @grant        GM_getResourceText
 // @grant        GM_registerMenuCommand
-// @homepage     https://github.com/s757129/FuckScripts
+// @homepage     https://s757129.github.io
 // @license      MIT
 // ==/UserScript==
 
@@ -32,47 +31,33 @@
     unsafeWindow.GM_getResourceText = GM_getResourceText;
     unsafeWindow.GM_registerMenuCommand = GM_registerMenuCommand;
 
-    //使用旧版首页
-    var localhref = unsafeWindow.location;
-    if(localhref.href.indexOf('c.biancheng.net/cpp')!=-1) {
-        localhref.replace("/");
-    }
-    if(localhref.href.indexOf('m.biancheng.net/cpp')!=-1) {
-        localhref.replace("/");
-    }
-
     //菜单
     let main = {
 
         //隐藏已知广告
         hidead() {
-            GM_addStyle('#top-banner,#q2a-fudao,a[href*="fudao"],#product-type li a[href*="fudao"],#nav-main li a[href*="fudao"],#arc-append,blockquote,#ggxc-weixin-arcbottom,#ggxc-weixin-listbottom,.ad-box{display:none; !important}');
-            //HTML注释
-            $('*').contents().each(function () {
-                if(this.nodeType === Node.COMMENT_NODE) {
-                    $(this).remove();
-                }
-            });
+            GM_addStyle('#top-banner, #q2a-fudao, a[href*="fudao"], #product-type li a[href*="fudao"], #nav-main li a[href*="fudao"], #arc-append, .tip-box, blockquote, #ggxc-weixin-arcbottom, #ggxc-weixin-listbottom, .ad-box { display: none; }');
         },
 
         //隐藏会员中心
         hidevip() {
-            GM_addStyle('#topbar,.user-info,#nav-main li a[href*="vip.biancheng.net"]{display:none; !important}');
+            GM_addStyle('#topbar, .user-info, #nav-main li a[href*="vip.biancheng.net"] { display: none; }');
         },
 
         //隐藏付费内容
         hidecost() {
-            setTimeout(function() {
-                $('a[href*="/view/vip"]').parent().hide();
-                if($('.tip-box').val("高级教程")) {
-                    $('.tip-box').hide();
-                }
-            }, 10);
+            let iconfontvip = document.querySelectorAll('.iconfont-vip2');
+            for (let i = 0; i < iconfontvip.length; i++) {
+                iconfontvip[i].parentNode.style.display = 'none';
+            }
         },
 
         //阻止新建标签
         disabledblank() {
-            $('a').removeAttr("target");
+            let rmatarget = document.querySelectorAll('a');
+            for (let i = 0; i < rmatarget.length; i++) {
+                rmatarget[i].removeAttribute('target');
+            }
         },
 
     };
@@ -115,14 +100,16 @@
         //CSS
         let SwalStyle = `
 .setting-container { z-index: 99999; !important }
-.setting-label { display: flex;align-items: center;justify-content: space-between;letter-spacing:2px;margin:11px 22px; }
-.switch-btn { cursor: pointer;width: 45px;height: 28px;position: relative;border: 1px solid #dfdfdf;background-color: #fdfdfd;box-shadow: #dfdfdf 0 0 0 0 inset;border-radius: 15px;background-clip: content-box;display: inline-block;-webkit-appearance: none;user-select: none;outline: none; }
-.switch-btn:before {content: '';width: 25px;height: 25px;position: absolute;top: 0;left: 0;border-radius: 20px;background-color: #fff;box-shadow: 0 1px 3px rgba(0, 0, 0, .4); }
-.switch-btn:checked { border-color: #7066e0;box-shadow: #7066e0 0 0 0 16px inset;background-color: #7066e0; }
+.swal2-popup { width: 23em; !important }
+.swal2-footer { font-size: 0.875em; !important }
+.setting-label { display: flex; align-items: center; justify-content: space-between; letter-spacing:2px; margin:11px 22px; }
+.switch-btn { cursor: pointer; width: 45px; height: 28px; position: relative; border: 1px solid #dfdfdf; background-color: #fdfdfd; box-shadow: #dfdfdf 0 0 0 0 inset; border-radius: 15px; background-clip: content-box; display: inline-block; -webkit-appearance: none; user-select: none; outline: none; }
+.switch-btn:before {content: ''; width: 25px; height: 25px; position: absolute; top: 0; left: 0; border-radius: 20px; background-color: #fff; box-shadow: 0 1px 3px rgba(0, 0, 0, .4); }
+.switch-btn:checked { border-color: #7066e0; box-shadow: #7066e0 0 0 0 16px inset; background-color: #7066e0; }
 .switch-btn:checked:before { left: 18px; }
 .switch-btn.switch-btn-animbg { transition: background-color ease .4s; }
 .switch-btn.switch-btn-animbg:before { transition: left .3s; }
-.switch-btn.switch-btn-animbg:checked { box-shadow: #dfdfdf 0 0 0 0 inset;background-color: #7066e0;transition: border-color .4s, background-color ease .4s; }
+.switch-btn.switch-btn-animbg:checked { box-shadow: #dfdfdf 0 0 0 0 inset; background-color: #7066e0; transition: border-color .4s, background-color ease .4s; }
 .switch-btn.switch-btn-animbg:checked:before { transition: left .3s; }
 		`;
 
@@ -145,22 +132,22 @@
             html: Swalhtml,
             showCloseButton: true,
             confirmButtonText: '保存',
-            footer: '<div style="text-align: center;font-size: 1em;">一起学习 <a href="https://bbs.tampermonkey.net.cn/thread-184-1-1.html" target="_blank" style="color:#7066e0;">油猴脚本开发</a> 吧，此脚本免费开源，Powered by <a href="https://github.com/s757129" target="_blank" style="color:#7066e0;font-weight:bold;">柒伍七</a></div>',
+            footer: '<div style="text-align: center;font-size: 1em;">一起学习 <a href="https://bbs.tampermonkey.net.cn/thread-184-1-1.html" target="_blank" style="color:#7066e0;">油猴脚本开发</a> 吧，此脚本免费开源<br>Powered by <a href="https://github.com/s757129" target="_blank" style="color:#7066e0;font-weight:bold;">柒伍七</a></div>',
         }).then((result) => {
             result.isConfirmed && history.go(0);
         });
 
         //Checkbox
-        document.getElementById('hide_ad').addEventListener('change', (e) => {
+        document.querySelector('#hide_ad').addEventListener('change', (e) => {
             GM_setValue('setting_hide_ad', e.target.checked);
         });
-        document.getElementById('hide_vip').addEventListener('change', (e) => {
+        document.querySelector('#hide_vip').addEventListener('change', (e) => {
             GM_setValue('setting_hide_vip', e.target.checked);
         });
-        document.getElementById('hide_cost').addEventListener('change', (e) => {
+        document.querySelector('#hide_cost').addEventListener('change', (e) => {
             GM_setValue('setting_hide_cost', e.target.checked);
         });
-        document.getElementById('disabled_blank').addEventListener('change', (e) => {
+        document.querySelector('#disabled_blank').addEventListener('change', (e) => {
             GM_setValue('setting_disabled_blank', e.target.checked);
         });
 
