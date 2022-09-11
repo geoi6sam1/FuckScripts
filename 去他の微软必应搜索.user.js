7c1a7b6789e82da36ae983fc7c6cbbf9c3c8db45
柒伍七 <2858727468@qq.com>
2022-09-11 13:05:38 +0800

    v0.1

---
+++ 去他の微软必应搜索.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         去他の微软必应搜索
+// @namespace    https://github.com/s757129/FuckScripts
+// @version      0.1
+// @description  屏蔽微软必应搜索广告加菊部美化
+// @author       柒伍七
+// @match        *://*.bing.com/search?q=*
+// @icon         https://cn.bing.com/favicon.ico
+// @run-at       document-idle
+// @grant        unsafeWindow
+// @grant        GM_addStyle
+// @homepage     https://s757129.github.io
+// @license      MIT
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    //unsafeWindow
+    unsafeWindow.GM_addStyle = GM_addStyle;
+
+    //新版广告
+    GM_addStyle('.b_ad { display: none; }');//.b_adTop,.b_adBottom
+
+    //旧版广告
+    let fuckad = document.querySelectorAll('.b_attribution[data-partnertag]');//data-tag
+    for (let i = 0; i < fuckad.length; i++) {
+        fuckad[i].parentNode.parentNode.style.display = 'none';
+    }
+
+})();
