a31e0dd76cc566993168e7591c27749a316260ed
柒伍七 <2858727468@qq.com>
2022-09-11 14:08:17 +0800

    v1.0.3

--- 去他の粤工程校园网.user.js
+++ 去他の粤工程校园网.user.js
@@ -1,6 +1,8 @@
 // ==UserScript==
 // @name         去他の粤工程校园网
-// @namespace    https://github.com/s757129/FuckScripts
+// @namespace    https://github.com/s757129
+// @homepage     https://s757129.github.io
+// @supportURL   https://github.com/s757129/FuckScripts
 // @version      1.0.3
 // @description  自动登录粤工程宿舍校园网
 // @author       柒伍七
@@ -8,7 +10,6 @@
 // @match        *://10.10.0.253/*
 // @icon         https://www.gdep.edu.cn/__local/1/78/44/67DBBD0E4AF7849771AEBA0A69A_8F5875B0_45D51.jpg?e=.jpg
 // @run-at       document-end
-// @homepage     https://s757129.github.io
 // @license      MIT
 // ==/UserScript==
  
