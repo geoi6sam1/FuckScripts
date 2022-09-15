64c8d6a9cfec4309e21a4b056bb0ee4c4848c100
柒伍七 <2858727468@qq.com>
2022-09-15 22:45:27 +0800

    Update 去他の哔哩哔哩直播签到.user.js

--- 去他の哔哩哔哩直播签到.user.js
+++ 去他の哔哩哔哩直播签到.user.js
@@ -1,72 +1,64 @@
 // ==UserScript==
-// @name         去他の哔哩哔哩直播签到
+// @name         去他の哔哩哔哩签到
 // @namespace    https://github.com/s757129
 // @homepage     https://s757129.github.io
 // @supportURL   https://github.com/s757129/FuckScripts
 // @version      0.1
-// @description  哔哩哔哩直播自动签到
+// @description  哔哩哔哩自动签到
 // @author       柒伍七
+// @match        *://*.bilibili.com/*
 // @icon         https://www.bilibili.com/favicon.ico
-// @crontab      * * once * *
-// @connect      api.live.bilibili.com
-// @cloudCat
+// @run-at       document-end
 // @license      What The Hell
 // ==/UserScript==
 
-return new Promise((resolve, reject) => {
-
-    fetch("https://api.bilibili.com/x/web-interface/nav", {
-        method: "GET",
-        mode: 'cors',
-        credentials: 'include',
-        headers: {
-            'cookie': document.cookie.split(';'),
+fetch("https://api.bilibili.com/x/web-interface/nav", {
+    method: "GET",
+    mode: 'cors',
+    credentials: 'include',
+    headers: {
+        'cookie': document.cookie.split(';'),
+    }
+}).then(response => response.json())
+    .then(response => {
+        let login = response.data.isLogin;
+        let code = response.code;
+        let name = response.data.uname;
+        switch (login) {
+            case true:
+                console.log('Code: ' + code + '\nUser: ' + name);
+                break;
+            case false:
+                console.log('Code: ' + code + '\nTips: 请登录后再试!');
+                break;
         }
-    }).then(response => response.json())
-        .then(response => {
-            let login = response.data.isLogin;
-            let code = response.code;
-            let name = response.data.uname;
-            switch (login) {
-                case true:
-                    console.log('Code: ' + code + '\nUser: ' + name);
-                    break;
-                case false:
-                    console.log('Code: ' + code + '\nTips: 请登录后再试!');
-                    break;
-            }
 
-        })
+    })
 
-    fetch("https://api.live.bilibili.com/xlive/web-ucenter/v1/sign/DoSign", {
-        method: "GET",
-        mode: 'cors',
-        credentials: 'include',
-        headers: {
-            'cookie': document.cookie.split(';'),
-        }
-    }).then(response => response.json())
-        .then(response => {
-            let code = response.code;
-            let msg = response.message;
-            let log = 'Code: ' + code + '\nTips: ' + msg;
-            console.log(log);
-        })
+fetch("https://api.live.bilibili.com/xlive/web-ucenter/v1/sign/DoSign", {
+    method: "GET",
+    mode: 'cors',
+    credentials: 'include',
+    headers: {
+        'cookie': document.cookie.split(';'),
+    }
+}).then(response => response.json())
+    .then(response => {
+        let code = response.code;
+        let msg = response.message;
+        console.log('Code: ' + code + '\nTips: ' + msg);
+    })
 
-    fetch("https://api.live.bilibili.com/xlive/web-ucenter/v1/sign/WebGetSignInfo", {
-        method: "GET",
-        mode: 'cors',
-        credentials: 'include',
-        headers: {
-            'cookie': document.cookie.split(';'),
-        }
-    }).then(response => response.json())
-        .then(response => {
-            let txt = response.data.text;
-            let stxt = response.data.specialText;
-            let log = '今日签到获得' + txt + ',' + stxt;
-            console.log(log);
-        })
-    
-    resolve();
-});
+fetch("https://api.live.bilibili.com/xlive/web-ucenter/v1/sign/WebGetSignInfo", {
+    method: "GET",
+    mode: 'cors',
+    credentials: 'include',
+    headers: {
+        'cookie': document.cookie.split(';'),
+    }
+}).then(response => response.json())
+    .then(response => {
+        let txt = response.data.text;
+        let stxt = response.data.specialText;
+        console.log('今日签到获得' + txt + ',' + stxt);
+    })
