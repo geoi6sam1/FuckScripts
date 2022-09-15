ec8860a3235598de11db282fa7eaf8926a08293d
柒伍七 <2858727468@qq.com>
2022-09-15 22:56:30 +0800

    Update 去他の油猴中文网.user.js

--- 去他の油猴中文网.user.js
+++ 去他の油猴中文网.user.js
@@ -14,23 +14,18 @@
 // @license      What The Hell
 // ==/UserScript==
 
-(function () {
-    'use strict';
+//unsafeWindow
+unsafeWindow.GM_addStyle = GM_addStyle;
 
-    //unsafeWindow
-    unsafeWindow.GM_addStyle = GM_addStyle;
+//隐藏已知广告
+GM_addStyle('.comiis_nv_pop, .a_f, .a_p { display: none; }');
 
-    //隐藏已知广告
-    GM_addStyle('.comiis_nv_pop, .a_f, .a_p { display: none; }');
-
-    //去除复制链接尾缀
-    //原创：李恒道
-    //原文：https://bbs.tampermonkey.net.cn/thread-1788-1-1.html
-    setTimeout(function () {
-        let oldcopy = unsafeWindow.setCopy;
-        unsafeWindow.setCopy = function (text, msg) {
-            return oldcopy.call(this, text.replace('\n(出处: 油猴中文网)\n', ""), msg)
-        }
-    }, 1000);
-
-})();
+//去除复制链接尾缀
+//原创：李恒道
+//原文：https://bbs.tampermonkey.net.cn/thread-1788-1-1.html
+setTimeout(function () {
+    let oldcopy = unsafeWindow.setCopy;
+    unsafeWindow.setCopy = function (text, msg) {
+        return oldcopy.call(this, text.replace('\n(出处: 油猴中文网)\n', ""), msg)
+    }
+}, 1000);
