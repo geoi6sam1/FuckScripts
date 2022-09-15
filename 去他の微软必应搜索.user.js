b1700f1c96dc02e2ca24ed541b0430b1ed782848
柒伍七 <2858727468@qq.com>
2022-09-15 22:55:43 +0800

    Update 去他の微软必应搜索.user.js

--- 去他の微软必应搜索.user.js
+++ 去他の微软必应搜索.user.js
@@ -20,123 +20,120 @@
 // @license      What The Hell
 // ==/UserScript==
 
-(function () {
-    'use strict';
+//unsafeWindow
+unsafeWindow.GM_addStyle = GM_addStyle;
+unsafeWindow.GM_getValue = GM_getValue;
+unsafeWindow.GM_setValue = GM_setValue;
+unsafeWindow.GM_getResourceText = GM_getResourceText;
+unsafeWindow.GM_registerMenuCommand = GM_registerMenuCommand;
 
-    //unsafeWindow
-    unsafeWindow.GM_addStyle = GM_addStyle;
-    unsafeWindow.GM_getValue = GM_getValue;
-    unsafeWindow.GM_setValue = GM_setValue;
-    unsafeWindow.GM_getResourceText = GM_getResourceText;
-    unsafeWindow.GM_registerMenuCommand = GM_registerMenuCommand;
+//菜单
+let main = {
 
-    //菜单
-    let main = {
+    /*** 隐藏已知广告 ***/
+    hidead() {
+        //新版广告
+        GM_addStyle('.b_ad, ul[data-partnertag] { display: none; }');
 
-        /*** 隐藏已知广告 ***/
-        hidead() {
-            //新版广告
-            GM_addStyle('.b_ad, ul[data-partnertag] { display: none; }');
+        //旧版广告
+        let fuckad = document.querySelectorAll('.b_attribution[data-partnertag]');
+        for (let i = 0; i < fuckad.length; i++) {
+            fuckad[i].parentNode.parentNode.style.display = 'none';
+        }
 
-            //旧版广告
-            let fuckad = document.querySelectorAll('.b_attribution[data-partnertag]');
-            for (let i = 0; i < fuckad.length; i++) {
-                fuckad[i].parentNode.parentNode.style.display = 'none';
+        //显示相关视频
+        let bottom_sp = document.querySelector('#serpvidans');
+        if (bottom_sp) {
+            bottom_sp.style.display = 'block';
+        }
+    },
+
+    /*** 隐藏相关视频 ***/
+    hidesp() {
+        let bottom_sp = document.querySelector('#serpvidans');
+        if (bottom_sp) {
+            bottom_sp.parentNode.parentNode.style.display = 'none';
+            bottom_sp.style.display = 'block';
+        }
+        let phone_sp = document.querySelector('#vidans2');
+        if (phone_sp) {
+            phone_sp.parentNode.parentNode.style.display = 'none';
+        }
+    },
+
+    /*** 隐藏人们还会问 ***/
+    hiderw() {
+        let bottom_rw = document.querySelector('#df_listaa');
+        if (bottom_rw) {
+            if (navigator.userAgent.indexOf('Android') > -1 || navigator.userAgent.indexOf('iPhone') > -1 || navigator.userAgent.indexOf('iPad') > -1 || navigator.userAgent.indexOf('iPod') > -1 || navigator.userAgent.indexOf('Symbian') > -1) {
+                bottom_rw.parentNode.parentNode.parentNode.parentNode.style.display = 'none';
+            } else {
+                bottom_rw.parentNode.parentNode.parentNode.style.display = 'none';
             }
+        }
+    },
 
-            //显示相关视频
-            let bottom_sp = document.querySelector('#serpvidans');
-            if (bottom_sp) {
-                bottom_sp.style.display = 'block';
-            }
-        },
+    /*** 隐藏底部相关搜索 ***/
+    hiders() {
+        let bottom_rs = document.querySelector('.b_rs');
+        if (bottom_rs) {
+            bottom_rs.parentNode.style.display = 'none';
+        }
+    },
 
-        /*** 隐藏相关视频 ***/
-        hidesp() {
-            let bottom_sp = document.querySelector('#serpvidans');
-            if (bottom_sp) {
-                bottom_sp.parentNode.parentNode.style.display = 'none';
-                bottom_sp.style.display = 'block';
-            }
-            let phone_sp = document.querySelector('#vidans2');
-            if (phone_sp) {
-                phone_sp.parentNode.parentNode.style.display = 'none';
-            }
-        },
+    /*** 隐藏最新相关信息(资讯) ***/
+    hidezx() {
+        let between_zx = document.querySelector('#ans_nws');
+        if (between_zx) {
+            between_zx.parentNode.style.display = 'none';
+        }
+    },
 
-        /*** 隐藏人们还会问 ***/
-        hiderw() {
-            let bottom_rw = document.querySelector('#df_listaa');
-            if (bottom_rw) {
-                if (navigator.userAgent.indexOf('Android') > -1 || navigator.userAgent.indexOf('iPhone') > -1 || navigator.userAgent.indexOf('iPad') > -1 || navigator.userAgent.indexOf('iPod') > -1 || navigator.userAgent.indexOf('Symbian') > -1) {
-                    bottom_rw.parentNode.parentNode.parentNode.parentNode.style.display = 'none';
-                } else {
-                    bottom_rw.parentNode.parentNode.parentNode.style.display = 'none';
-                }
-            }
-        },
+};
 
-        /*** 隐藏底部相关搜索 ***/
-        hiders() {
-            let bottom_rs = document.querySelector('.b_rs');
-            if (bottom_rs) {
-                bottom_rs.parentNode.style.display = 'none';
-            }
-        },
+//判断配置
+if (GM_getValue('setting_hide_ad')) {
+    main.hidead();
+};
+if (GM_getValue('setting_hide_sp')) {
+    main.hidesp();
+};
+if (GM_getValue('setting_hide_rw')) {
+    main.hiderw();
+};
+if (GM_getValue('setting_hide_rs')) {
+    main.hiders();
+};
+if (GM_getValue('setting_hide_zx')) {
+    main.hidezx();
+};
 
-        /*** 隐藏最新相关信息(资讯) ***/
-        hidezx() {
-            let between_zx = document.querySelector('#ans_nws');
-            if (between_zx) {
-                between_zx.parentNode.style.display = 'none';
-            }
-        },
+//默认配置
+let value = [{
+    name: 'setting_hide_ad',
+    value: true
+}, {
+    name: 'setting_hide_sp',
+    value: true
+}, {
+    name: 'setting_hide_rw',
+    value: true
+}, {
+    name: 'setting_hide_rs',
+    value: true
+}, {
+    name: 'setting_hide_zx',
+    value: true
+}];
+value.forEach((v) => {
+    GM_getValue(v.name) === undefined && GM_setValue(v.name, v.value);
+});
 
-    };
+//设置
+GM_registerMenuCommand('⚙️ 设置', () => {
 
-    //判断配置
-    if (GM_getValue('setting_hide_ad')) {
-        main.hidead();
-    };
-    if (GM_getValue('setting_hide_sp')) {
-        main.hidesp();
-    };
-    if (GM_getValue('setting_hide_rw')) {
-        main.hiderw();
-    };
-    if (GM_getValue('setting_hide_rs')) {
-        main.hiders();
-    };
-    if (GM_getValue('setting_hide_zx')) {
-        main.hidezx();
-    };
-
-    //默认配置
-    let value = [{
-        name: 'setting_hide_ad',
-        value: true
-    }, {
-        name: 'setting_hide_sp',
-        value: true
-    }, {
-        name: 'setting_hide_rw',
-        value: true
-    }, {
-        name: 'setting_hide_rs',
-        value: true
-    }, {
-        name: 'setting_hide_zx',
-        value: true
-    }];
-    value.forEach((v) => {
-        GM_getValue(v.name) === undefined && GM_setValue(v.name, v.value);
-    });
-
-    //设置
-    GM_registerMenuCommand('⚙️ 设置', () => {
-
-        //CSS
-        let SwalStyle = `
+    //CSS
+    let SwalStyle = `
 .setting-container { z-index: 99999; }
 .swal2-popup { width: 26em; }
 .swal2-footer { font-size: 0.875em; }
@@ -152,8 +149,8 @@
 .switch-btn.switch-btn-animbg:checked:before { transition: left .3s; }
 		`;
 
-        //HTML
-        let Swalhtml = `
+    //HTML
+    let Swalhtml = `
 <label class="setting-label">隐藏已知广告<input id="hide_ad" ${GM_getValue('setting_hide_ad') ? 'checked' : ''} type="checkbox" class="switch-btn switch-btn-animbg" /></label>
 <label class="setting-label">隐藏相关视频<input id="hide_sp" ${GM_getValue('setting_hide_sp') ? 'checked' : ''} type="checkbox" class="switch-btn switch-btn-animbg" /></label>
 <label class="setting-label">隐藏人们还会问<input id="hide_rw" ${GM_getValue('setting_hide_rw') ? 'checked' : ''} type="checkbox" class="switch-btn switch-btn-animbg" /></label>
@@ -161,39 +158,37 @@
 <label class="setting-label">隐藏最新相关信息(资讯)<input id="hide_zx" ${GM_getValue('setting_hide_zx') ? 'checked' : ''} type="checkbox" class="switch-btn switch-btn-animbg" /></label>
     	`;
 
-        //载入资源
-        GM_addStyle(GM_getResourceText('SwalCSS'));
-        GM_addStyle(SwalStyle);
+    //载入资源
+    GM_addStyle(GM_getResourceText('SwalCSS'));
+    GM_addStyle(SwalStyle);
 
-        //SweetAlert2
-        Swal.fire({
-            icon: 'info',
-            title: '<strong>自定义配置</strong>',
-            html: Swalhtml,
-            showCloseButton: true,
-            confirmButtonText: '保存',
-            footer: '<div style="text-align: center;font-size: 1em;">一起学习 <a href="https://bbs.tampermonkey.net.cn/thread-184-1-1.html" target="_blank" style="color:#7066e0;">油猴脚本开发</a> 吧，此脚本免费开源<br>Powered by <a href="https://github.com/s757129" target="_blank" style="color:#7066e0;font-weight:bold;">柒伍七</a></div>',
-        }).then((result) => {
-            result.isConfirmed && history.go(0);
-        });
-
-        //Checkbox
-        document.querySelector('#hide_ad').addEventListener('change', (e) => {
-            GM_setValue('setting_hide_ad', e.target.checked);
-        });
-        document.querySelector('#hide_sp').addEventListener('change', (e) => {
-            GM_setValue('setting_hide_sp', e.target.checked);
-        });
-        document.querySelector('#hide_rw').addEventListener('change', (e) => {
-            GM_setValue('setting_hide_rw', e.target.checked);
-        });
-        document.querySelector('#hide_rs').addEventListener('change', (e) => {
-            GM_setValue('setting_hide_rs', e.target.checked);
-        });
-        document.querySelector('#hide_zx').addEventListener('change', (e) => {
-            GM_setValue('setting_hide_zx', e.target.checked);
-        });
-
+    //SweetAlert2
+    Swal.fire({
+        icon: 'info',
+        title: '<strong>自定义配置</strong>',
+        html: Swalhtml,
+        showCloseButton: true,
+        confirmButtonText: '保存',
+        footer: '<div style="text-align: center;font-size: 1em;">一起学习 <a href="https://bbs.tampermonkey.net.cn/thread-184-1-1.html" target="_blank" style="color:#7066e0;">油猴脚本开发</a> 吧，此脚本免费开源<br>Powered by <a href="https://github.com/s757129" target="_blank" style="color:#7066e0;font-weight:bold;">柒伍七</a></div>',
+    }).then((result) => {
+        result.isConfirmed && history.go(0);
     });
 
-})();
+    //Checkbox
+    document.querySelector('#hide_ad').addEventListener('change', (e) => {
+        GM_setValue('setting_hide_ad', e.target.checked);
+    });
+    document.querySelector('#hide_sp').addEventListener('change', (e) => {
+        GM_setValue('setting_hide_sp', e.target.checked);
+    });
+    document.querySelector('#hide_rw').addEventListener('change', (e) => {
+        GM_setValue('setting_hide_rw', e.target.checked);
+    });
+    document.querySelector('#hide_rs').addEventListener('change', (e) => {
+        GM_setValue('setting_hide_rs', e.target.checked);
+    });
+    document.querySelector('#hide_zx').addEventListener('change', (e) => {
+        GM_setValue('setting_hide_zx', e.target.checked);
+    });
+
+});
