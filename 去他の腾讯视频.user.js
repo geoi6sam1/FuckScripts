ee7106af9544ed8e6bc438739fe714773c727945
柒伍七 <2858727468@qq.com>
2022-10-25 15:18:19 +0800

    Update 去他の腾讯视频.user.js

--- 去他の腾讯视频.user.js
+++ 去他の腾讯视频.user.js
@@ -1,13 +1,12 @@
 // ==UserScript==
 // @name         去他の腾讯视频
 // @namespace    https://github.com/s757129
-// @homepage     https://s757129.github.io
-// @supportURL   https://github.com/s757129/FuckScripts
 // @version      0.1
 // @description  隐藏腾讯视频观影页面影响观影体验的内容
 // @author       柒伍七
 // @match        *://v.qq.com/x/cover/*
 // @icon         https://v.qq.com/favicon.ico
+// @supportURL   https://github.com/s757129/FuckScripts/issues
 // @run-at       document-idle
 // @grant        unsafeWindow
 // @grant        GM_addStyle
