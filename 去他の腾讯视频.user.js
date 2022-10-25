836dd5d4aa13ae03ce949d3aab374708348bad8d
柒伍七 <2858727468@qq.com>
2022-10-25 17:04:19 +0800

    Update 去他の腾讯视频.user.js

--- 去他の腾讯视频.user.js
+++ 去他の腾讯视频.user.js
@@ -2,9 +2,9 @@
 // @name         去他の腾讯视频
 // @namespace    https://github.com/s757129
 // @version      0.1
-// @description  隐藏腾讯视频观影页面影响观影体验的内容
+// @description  隐藏腾讯视频影响观影体验的内容建议搭配VIP食用
 // @author       柒伍七
-// @match        *://v.qq.com/x/cover/*
+// @match        *://v.qq.com/*
 // @icon         https://v.qq.com/favicon.ico
 // @supportURL   https://github.com/s757129/FuckScripts/issues
 // @run-at       document-idle
@@ -17,4 +17,4 @@
 unsafeWindow.GM_addStyle = GM_addStyle;
 
 //GM_addStyle
-GM_addStyle(`.vip-button, #iwan-game-switch-page, .txp-watermark, txpdiv.txp_zt {display: none !important;}`);
+GM_addStyle(`.quick_item.quick_games, .quick_item.quick_upload, .mod_row_box.mod_row_box_special, #ad_container, .vip-button, #iwan-game-switch-page, .txp-watermark, txpdiv.txp_zt {display: none !important;}`);
