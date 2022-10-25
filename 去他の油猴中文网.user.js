22c22208aed08d90e1fb014799a5aeb1b8bcf3dc
柒伍七 <2858727468@qq.com>
2022-10-25 15:20:21 +0800

    Update 去他の油猴中文网.user.js

--- 去他の油猴中文网.user.js
+++ 去他の油猴中文网.user.js
@@ -1,17 +1,16 @@
 // ==UserScript==
 // @name         去他の油猴中文网
 // @namespace    https://github.com/s757129
-// @homepage     https://s757129.github.io
-// @supportURL   https://github.com/s757129/FuckScripts
 // @version      1.0.4
 // @description  屏蔽油猴中文网广告加菊部美化
 // @author       柒伍七
 // @match        *://bbs.tampermonkey.net.cn/*
 // @icon         https://bbs.tampermonkey.net.cn/favicon.ico
+// @supportURL   https://github.com/s757129/FuckScripts/issues
 // @run-at       document-start
 // @grant        unsafeWindow
 // @grant        GM_addStyle
-// @license      What The Hell
+// @license      MIT
 // ==/UserScript==
 
 //unsafeWindow
