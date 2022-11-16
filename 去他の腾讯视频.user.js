4842406435d73a5bc6151f5a1546a23c8cb69ff9
柒伍七 <2858727468@qq.com>
2022-11-16 17:27:41 +0800

    Update 去他の腾讯视频.user.js

--- 去他の腾讯视频.user.js
+++ 去他の腾讯视频.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         去他の腾讯视频
 // @namespace    https://github.com/s757129
-// @version      0.4
+// @version      0.5
 // @description  屏蔽腾讯视频广告加菊部美化
 // @author       柒伍七
 // @match        *://v.qq.com/*
@@ -19,13 +19,11 @@ unsafeWindow.GM_addStyle = GM_addStyle;
 
 //非跳过视频开头广告脚本，请搭配VIP食用
 GM_addStyle(`
+.quick_vip,
 .quick_games,
 .quick_upload,
-.quick_vip,
 #pc_client,
 [dt-params*="ad_"],
-.site_board_ads_inner,
-.mod_row_box_special,
 .nav-wrap a[href*="gamer"],
 .nav-wrap a[href*="iwan"],
 .tip_download,
@@ -34,8 +32,8 @@ GM_addStyle(`
 .vip-button,
 #iwan-game-switch-page,
 .iwan-wrapper,
-txpdiv[data-role*="ad-"],
-txpdiv[data-role*="watermark"]
+[data-role*="ad-"],
+[data-role*="watermark"]
 {
     display: none !important;
 }
