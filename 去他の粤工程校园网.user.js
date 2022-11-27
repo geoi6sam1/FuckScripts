b8de1b0a035b7a43dc8dbc72dbc600da81a85efb
柒伍七 <2858727468@qq.com>
2022-11-27 18:47:17 +0800

    Update 去他の粤工程校园网.user.js

--- 去他の粤工程校园网.user.js
+++ 去他の粤工程校园网.user.js
@@ -6,26 +6,31 @@
 // @author       柒伍七
 // @match        *://10.10.0.76/*
 // @match        *://10.10.0.253/*
-// @icon         https://www.gdep.edu.cn/__local/1/78/44/67DBBD0E4AF7849771AEBA0A69A_8F5875B0_45D51.jpg?e=.jpg
+// @icon         https://www.gdep.edu.cn/__local/1/78/44/67DBBD0E4AF7849771AEBA0A69A_8F5875B0_45D51.jpg
 // @supportURL   https://github.com/s757129/FuckScripts/issues
 // @run-at       document-start
 // @license      MIT
 // ==/UserScript==
 
-var uid = ''; // 学号
-var pwd = ''; // 密码
+let uid = ''; // 学号
+let pwd = ''; // 密码
 
-setTimeout(() => {
-    var userid = document.querySelector('#userid');
-    var passwd = document.querySelector('#passwd');
+let login = setInterval(() => {
+    let userid = document.querySelector('#userid');
+    let passwd = document.querySelector('#passwd');
     if (userid && passwd != null) {
         userid.value = uid;
         passwd.value = pwd;
         setTimeout(() => {
-            var loginsubmit = document.querySelector('#loginsubmit');
+            let loginsubmit = document.querySelector('#loginsubmit');
             if (loginsubmit != null) {
                 loginsubmit.click();
             }
         }, 456);
     }
-}, 123);
+}, 99);
+
+let logoutsubmit = document.querySelector('#logoutsubmit');
+if (logoutsubmit != null) {
+    clearInterval(login);
+}
