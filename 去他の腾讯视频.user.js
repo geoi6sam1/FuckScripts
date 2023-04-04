9d2bf0893bd804d3aeb6ce14def3625dc1abcc7e
柒伍七 <2858727468@qq.com>
2023-04-04 23:29:17 +0800

    Update 去他の腾讯视频.user.js

--- 去他の腾讯视频.user.js
+++ 去他の腾讯视频.user.js
@@ -1,35 +1,32 @@
 // ==UserScript==
 // @name         去他の腾讯视频
 // @namespace    geoi6sam1
-// @version      0.71
-// @description  屏蔽腾讯视频广告加菊部美化
+// @version      0.7
+// @description  仅用于优化观影体验，非跳过视频开头广告脚本，有需要请使用VIP
 // @author       柒伍七
 // @match        *://v.qq.com/*
 // @match        *://film.qq.com/*
 // @match        *://m.film.qq.com/*
-// @match        *://iwan.qq.com/*
-// @match        *://gamer.qq.com/*
 // @icon         https://v.qq.com/favicon.ico
 // @supportURL   https://github.com/geoi6sam1/FuckScripts/issues
 // @run-at       document-start
-// @grant        unsafeWindow
 // @grant        GM_addStyle
 // @license      MIT
 // ==/UserScript==
 
-// unsafeWindow
-unsafeWindow.GM_addStyle = GM_addStyle;
 
-// 非跳过视频开头广告脚本，请搭配VIP食用
 GM_addStyle(`
+#iwan-gamependant-page,
 .quick_vip,
 .quick_games,
 .quick_upload,
 #pc_client,
+.left-nav-wrap a[href*="iwan"],
 [dt-params*="ad_"],
 .tip_download,
 #ad_container,
 [class*="_ad_"],
+[class*="-ad"],
 [data-role*="ad-"],
 [data-role*="watermark"],
 [data-role*="creative-player-pause"],
