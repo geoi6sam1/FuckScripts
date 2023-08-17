47417834e7ef26ef77deab46ab9709f9bc5caa19
潘钜森 <geoi6sam1@qq.com>
2023-08-17 23:30:45 +0800

    Update 粤工程校园网自动登录.user.js

--- 粤工程校园网自动登录.user.js
+++ 粤工程校园网自动登录.user.js
@@ -3,7 +3,7 @@
 // @namespace    geoi6sam1
 // @version      0.2
 // @description  自动登录粤工程宿舍校园网
-// @author       钜森
+// @author       潘钜森
 // @match        *://10.10.0.76/*
 // @match        *://10.10.0.253/*
 // @icon         https://www.gdep.edu.cn/__local/1/78/44/67DBBD0E4AF7849771AEBA0A69A_8F5875B0_45D51.jpg
