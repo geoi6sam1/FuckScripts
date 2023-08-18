7bafd2ee08ebebf093bd58bcd61985189a5b98bc
潘钜森 <geoi6sam1@qq.com>
2023-08-18 11:13:55 +0800

    Update 自动删除QQ空间说说.user.js

--- 自动删除QQ空间说说.user.js
+++ 自动删除QQ空间说说.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         自动删除QQ空间说说
-// @namespace    geoi6sam1
+// @namespace    https://github.com/geoi6sam1
 // @version      0.1
 // @description  选择了莫后悔，失去了别追回，路还是要走滴，生活还是该继续
-// @author       潘钜森
+// @author       geoi6sam1
 // @match        *://user.qzone.qq.com/*
 // @icon         https://qzone.qq.com/favicon.ico
 // @supportURL   https://github.com/geoi6sam1/FuckScripts/issues
@@ -22,14 +22,14 @@ setTimeout(() => {
     if (!qz_shuoshuo) {
         custom_menu_swf('311');
     }
-}, 3000);
+}, 4321);
 
 function del_qz_shuoshuo() {
     var choosess = document.querySelectorAll(".del.del_btn")[0];
     if (choosess) {
         choosess.click();
     }
-    setTimeout(() => { del_qz_dialog() }, 1500)
+    setTimeout(() => { del_qz_dialog() }, 1234);
 }
 
 function del_qz_dialog() {
@@ -37,11 +37,11 @@ function del_qz_dialog() {
     if (deletess) {
         deletess.click();
     }
-    setTimeout(() => { del_qz_shuoshuo() }, 1500)
+    setTimeout(() => { del_qz_shuoshuo() }, 1234);
 }
 
-del_qz_shuoshuo()
+del_qz_shuoshuo();
 
 setInterval(() => {
     window.location.reload();
-}, 60000);
+}, 54321);
