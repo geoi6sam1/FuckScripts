3d81ac46a276951b22682ec6b2e5ba460ad5d1d8
潘钜森 <geoi6sam1@qq.com>
2023-08-18 11:54:33 +0800

    Update 自动删除QQ空间说说.user.js

--- 自动删除QQ空间说说.user.js
+++ 自动删除QQ空间说说.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         自动删除QQ空间说说
 // @namespace    https://github.com/geoi6sam1
-// @version      0.1
+// @version      0.1.1
 // @description  选择了莫后悔，失去了别追回，路还是要走滴，生活还是该继续
 // @author       geoi6sam1
 // @match        *://user.qzone.qq.com/*
@@ -43,5 +43,5 @@ function del_qz_dialog() {
 del_qz_shuoshuo();
 
 setInterval(() => {
-    window.location.reload();
+    window.location.reload(true);
 }, 54321);
