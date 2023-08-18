c95bf8922e7816d76229e91a452321d2af7c686a
潘钜森 <geoi6sam1@qq.com>
2023-08-18 13:43:03 +0800

    Update 腾讯视频优化助手.user.js

--- 腾讯视频优化助手.user.js
+++ 腾讯视频优化助手.user.js
@@ -5,6 +5,7 @@
 // @description  仅用于优化观影体验，非跳过视频开头广告脚本，有需要请使用VIP
 // @author       geoi6sam1
 // @match        *://v.qq.com/*
+// @match        *://m.v.qq.com/*
 // @match        *://film.qq.com/*
 // @match        *://m.film.qq.com/*
 // @icon         https://v.qq.com/favicon.ico
@@ -26,19 +27,29 @@ GM_addStyle(`
 .quick_upload,
 .quick_message,
 #pc_client,
-.left-nav-wrap a[href*="iwan"],
+[class*="left-nav"] a[href*="iwan"],
 [dt-params*="ad_"],
+[dt-eid*="ad_"],
+#iwan-gamependant-page,
 .tip_download,
+.mod_vip_nav .vip_act,
 .fixed_box,
 #ad_container,
+#iwan-game,
+.banner-ad,
 .txp_ad,
 .txp_none,
 .txp-watermark,
-[id*="iwan-game"],
+.game-switch-ad,
 [class*="txp_full_screen_pause"],
 [data-role*="creative-player-pause"],
 .barrage-control,
-.thumbplayer-barrage
+.thumbplayer-barrage,
+#ad_m-site,
+.open-app.old-open,
+[dt-eid="openbanner"],
+.bottom-wrapper,
+.at-app-banner
 {
     display: none !important;
 }
@@ -57,25 +68,25 @@ GM_addStyle(`
 
 window.onkeypress = (e) => {
     if (e.keyCode === 32) {
-        txv();
+        txv_ad_float_fuck();
     }
 }
 
 window.onmousedown = (e) => {
     if (e.button === 0) {
-        txv();
+        txv_ad_float_fuck();
     }
 }
 
-function txv() {
-    let txplrv = document.querySelectorAll("#player video")[0];
-    if (txplrv !== undefined) {
+function txv_ad_float_fuck() {
+    var txv_player_choose = document.querySelectorAll("#player video")[0];
+    if (txv_player_choose) {
         setTimeout(() => {
-            if (txplrv.paused === true) {
-                let txvctr = document.querySelector(".txp_videos_container");
-                txplrv.setAttribute("style", "position: absolute; width: 100%; height: 100%; top: 0; left: 0; object-fit: contain; z-index: 0; visibility: inherit;");
-                txvctr.setAttribute("style", "position: absolute; left: 0; top: 0; width: 100%; height: 100%; z-index: 0;");
+            if (txv_player_choose.paused === true) {
+                var txv_player_win = document.querySelector(".txp_videos_container");
+                txv_player_choose.setAttribute("style", "position: absolute; width: 100%; height: 100%; top: 0; left: 0; object-fit: contain; z-index: 0; visibility: inherit;");
+                txv_player_win.setAttribute("style", "position: absolute; left: 0; top: 0; width: 100%; height: 100%; z-index: 0;");
             }
-        }, 999)
+        }, 999);
     }
 }
