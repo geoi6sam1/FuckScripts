1903eef561764c2bd505ae6eb2ac420c8ccc098b
潘钜森 <geoi6sam1@qq.com>
2024-02-15 16:21:56 +0800

    Update 自用校园网自动登录.user.js

--- 自用校园网自动登录.user.js
+++ 自用校园网自动登录.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name         粤工程校园网自动登录
+// @name         自用校园网自动登录
 // @namespace    https://github.com/geoi6sam1
 // @version      0.1.0
-// @description  自动登录粤工程宿舍校园网
+// @description  自动登录宿舍校园网
 // @author       geoi6sam1
 // @match        *://10.10.0.76/*
 // @match        *://10.10.0.253/*
-// @icon         https://www.gdep.edu.cn/__local/1/78/44/67DBBD0E4AF7849771AEBA0A69A_8F5875B0_45D51.jpg
+// @icon         https://www.google.com/favicon.ico
 // @supportURL   https://github.com/geoi6sam1/FuckScripts/issues
 // @antifeature  ads
 // @antifeature  miner
@@ -18,21 +18,21 @@
 // @license      MIT
 // ==/UserScript==
 
-let uid = ""; // 学号
-let pwd = ""; // 密码
+const uid = "" // 学号
+const pwd = "" // 密码
 
-let login = setInterval(() => {
-    let userid = document.querySelector("#userid");
-    let passwd = document.querySelector("#passwd");
+setInterval(() => {
+    var userid = document.querySelector("#userid")
+    var passwd = document.querySelector("#passwd")
     if (userid && passwd) {
-        userid.value = uid;
-        passwd.value = pwd;
-        let loginsubmit = document.querySelector("#loginsubmit");
+        userid.value = uid
+        passwd.value = pwd
+        let loginsubmit = document.querySelector("#loginsubmit")
         if (loginsubmit) {
-            loginsubmit.click();
+            loginsubmit.click()
         }
     }
-}, 99);
+}, 99)
 
 let logoutsubmit = document.querySelector("#logoutsubmit");
 if (logoutsubmit) {
