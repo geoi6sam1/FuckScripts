c5ebe53112ee4d08a5881c9670bf8b88e0f18bbd
潘钜森 <geoi6sam1@qq.com>
2024-05-12 11:39:15 +0800

    Update 腾讯视频优化助手.user.js

--- 腾讯视频优化助手.user.js
+++ 腾讯视频优化助手.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         腾讯视频优化助手
 // @namespace    https://github.com/geoi6sam1
-// @version      1.1.3
+// @version      1.1.4
 // @description  仅用于优化观影体验，非跳过视频开头广告脚本，有需要请开通腾讯视频会员或使用广告拦截扩展
 // @author       geoi6sam1@qq.com
 // @match        http*://v.qq.com/*
