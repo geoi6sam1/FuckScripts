c4b99a61d7749635cc8257982a8b8b42059df3e1
潘钜森 <geoi6sam1@qq.com>
2024-06-10 20:34:02 +0800

    Update 微软积分商城签到.user.js

--- 微软积分商城签到.user.js
+++ 微软积分商城签到.user.js
@@ -56,14 +56,14 @@ function getRandStr(type) {
             "https://top.baidu.com/api/board?tab=finance"
         ],
         pc: [
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6099.310 Safari/537.36 Edg/120.0.2210.175",
-            "Mozilla/5.0 (Sonoma; Intel Mac OS X 14_4_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.6045.214 Safari/537.36 Edg/119.0.2151.97",
-            "Mozilla/5.0 (X11; Deepin; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.5481.213 Safari/537.36 Edg/110.0.1587.78"
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0",
+            "Mozilla/5.0 (Sonoma; Intel Mac OS X 14_4_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
+            "Mozilla/5.0 (X11; Deepin; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.0.0"
         ],
         m: [
-            "Mozilla/5.0 (Linux; Android 14; MI 6 Build/UP1A.231005.007) Version/4.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.6312.134 Mobile Safari/537.36 EdgA/123.0.2420.97",
-            "Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1 EdgiOS/111.0.1661.62",
-            "Mozilla/5.0 (Linux; Android 10; HarmonyOS; ALN-AL10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.74 Mobile Safari/537.36 EdgA/101.0.1210.53"
+            "Mozilla/5.0 (Linux; Android 14; MI 6 Build/UP1A.231005.007) Version/4.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Mobile Safari/537.36 EdgA/123.0.0.0",
+            "Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1 EdgiOS/111.0.0.0",
+            "Mozilla/5.0 (Linux; Android 10; HarmonyOS; ALN-AL10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Mobile Safari/537.36 EdgA/101.0.0.0"
         ]
     }
     switch (type) {
@@ -209,7 +209,7 @@ let testTimes = 0
 
 async function taskPromotions() {
     if (testTimes > 2) {
-        pushMsg("活动任务失败", "失败！开始必应搜索任务，请继续耐心等待...")
+        pushMsg("活动任务失败", "失败！开始必应搜索任务，请耐心等待...")
         return true
     }
     const token = await getRewardsToken()
@@ -233,7 +233,7 @@ async function taskPromotions() {
             }
         }
         if (promotionsArr.length == 0) {
-            pushMsg("活动任务完成", "完成！开始必应搜索任务，请继续耐心等待...")
+            pushMsg("活动任务完成", "完成！开始必应搜索任务，请耐心等待...")
             return true
         } else {
             promotionsArr.forEach((item) => {
@@ -257,7 +257,7 @@ return new Promise((resolve, reject) => {
     const start = async () => {
         try {
             const result = await taskSearch()
-            result ? resolve() : setTimeout(start, getSRandNum(5432, 6789))
+            result ? resolve() : setTimeout(start, getSRandNum(5432, 6987))
         } catch (err) {
             reject(err)
         }
