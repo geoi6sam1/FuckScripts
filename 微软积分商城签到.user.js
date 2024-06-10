5dd4a09ab32a4f921d50aaee5b2dbd5945b28e79
潘钜森 <geoi6sam1@qq.com>
2024-06-11 00:24:22 +0800

    Update 微软积分商城签到.user.js

--- 微软积分商城签到.user.js
+++ 微软积分商城签到.user.js
@@ -57,19 +57,19 @@ function getRandStr(type) {
         ],
         pc: [
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0",
-            "Mozilla/5.0 (Sonoma; Intel Mac OS X 14_4_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
+            "Mozilla/5.0 (Sonoma; Intel Mac OS X 14_4_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/604.1 Edg/120.0.0.0",
             "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0"
         ],
-        m: [
+        mobile: [
             "Mozilla/5.0 (Linux; Android 14; MI 6 Build/UP1A.231005.007) Version/4.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36 EdgA/111.0.0.0",
-            "Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1 EdgiOS/110.0.0.0",
+            "Mozilla/5.0 (iPhone; CPU iPhone OS 16_7_8 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Mobile/15E148 Safari/604.1 EdgiOS/110.0.0.0",
             "Mozilla/5.0 (Linux; Android 10; HarmonyOS; ALN-AL10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36 EdgA/109.0.0.0"
         ]
     }
     switch (type) {
         case 0: return randData.url[getRandNum(randData.url.length)]
         case 1: return randData.pc[getRandNum(randData.pc.length)]
-        case 2: return randData.m[getRandNum(randData.m.length)]
+        case 2: return randData.mobile[getRandNum(randData.mobile.length)]
     }
 }
 
