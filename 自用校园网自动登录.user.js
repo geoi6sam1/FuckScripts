18f88f4e6a2940e0cbf2f5fc03853ab0eed3d609
潘钜森 <geoi6sam1@qq.com>
2025-02-15 15:34:20 +0800

    Update 自用校园网自动登录.user.js

--- 自用校园网自动登录.user.js
+++ 自用校园网自动登录.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name            自用校园网自动登录
-// @namespace       https://github.com/geoi6sam1
+// @namespace       https://github.com/geoisam
 // @version         0.1.0
 // @description     自动登录宿舍校园网
-// @author          geoi6sam1
+// @author          geoisam
 // @match           *://10.10.0.76/*
 // @match           *://10.10.0.253/*
 // @icon            https://www.tsinghua.edu.cn/favicon.ico
-// @supportURL      https://github.com/geoi6sam1/FuckScripts/issues
+// @supportURL      https://github.com/geoisam/FuckScripts/issues
 // @run-at          document-end
 // @license         GPL-3.0
 // ==/UserScript==
